42a1b7c43e231921648c8606333a288f09c5d7c2
ug123458 <ug123458@gmail.com>
2023-07-10 11:15:59 +0530

    initial commit

---
+++ helpers/jwt.js
@@ -0,0 +1,27 @@
+const { expressjwt: jwt } = require("express-jwt");
+
+function authJwt() {
+  const secret = process.env.secret;
+  const api = process.env.API_URL;
+
+  return jwt({
+    secret,
+    algorithms: ["HS256"],
+    isRevoked: isRevoked,
+  }).unless({
+    path: [
+      // { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
+      // { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+      // { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+      // `${api}/users/login`,
+      // `${api}/users/register`,
+      { url: /(.*)/ },
+    ],
+  });
+}
+
+async function isRevoked(req, { payload }) {
+  return !payload.isAdmin;
+}
+
+module.exports = authJwt;
